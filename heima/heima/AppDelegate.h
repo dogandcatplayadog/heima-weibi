fbce6cf44384157dfcb066c854e30e35d4091c11
dogandcatplayadog <13631611448@163.com>
2016-06-29 21:19:09 +0800

    init

---
+++ heima/heima/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  heima
+//
+//  Created by tang on 16/6/29.
+//  Copyright © 2016年 TangZhiHui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
